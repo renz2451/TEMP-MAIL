fe76bf0e0073f8a5037eac1c12ce65e12574a2ec
renz2451 <166629964+renz2451@users.noreply.github.com>
2026-04-29 14:03:37 +0800

    Update allowed host for configuration

--- js/config.js
+++ js/config.js
@@ -11,7 +11,7 @@
         sessionStorage.setItem('TEMP_MAIL_DEV_OVERRIDE', '1');
     }
     const devOverride = sessionStorage.getItem('TEMP_MAIL_DEV_OVERRIDE') === '1';
-    const ALLOWED_HOST = ['mehmetkahya0.github.io', '127.0.0.1'];
+    const ALLOWED_HOST = ['renz2451.github.io', '127.0.0.1'];
     if (!devOverride && !ALLOWED_HOST.includes(location.host) && !ALLOWED_HOST.includes(location.hostname)) {
         throw new Error('UNAUTHORIZED_HOST');
     }
